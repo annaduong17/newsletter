c8cd633363cdf8888baa2a99c0c9662549760ca6
Anna Duong <aduong9417@gmail.com>
2023-12-22 12:30:42 -0800

    add event listener for resize to render appropriate image

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import SubscribePage from './SubscribePage';
 import SuccessPage from './SuccessPage';
-import Illustration from '../images/illustration-sign-up-desktop.svg';
+import IllustrationDesktop from '../images/illustration-sign-up-desktop.svg';
+import IllustrationMobile from '../images/illustration-sign-up-mobile.svg'
 
 function App() {
   const [ email, setEmail ] = useState('');
   const [ formSubmitted, setFormSubmitted ] = useState(false);
+  const illustration = useRef();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,13 +17,29 @@ function App() {
     e.preventDefault();
     setFormSubmitted(true);
   };
+
+  const updateIllustration = () => {
+    if (window.innerWidth < 800) {
+      illustration.current.src = IllustrationMobile;
+    } else {
+      illustration.current.src = IllustrationDesktop;
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', updateIllustration);
+
+    return () => {
+      window.removeEventListener('resize', updateIllustration);
+    };
+  });
   
 
   return(
     <div className='container flex-center'>
       {formSubmitted ? <SuccessPage email={email} /> : <div className='main-content'>
         <SubscribePage email={email} onChange={handleEmailChange} onSubmit={handleOnSubmit}/>
-        <img id='illustration' src={Illustration} alt="illustration" />
+        <img ref={illustration} id='illustration' src={IllustrationDesktop} alt="illustration" />
       </div>}
     </div>
   )
