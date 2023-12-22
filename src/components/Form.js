67fe07331e037315bed835c318fa2c99be594808
Anna Duong <aduong9417@gmail.com>
2023-12-22 13:49:40 -0800

    validate email input

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import Button from './Button';
 
 export default function Form({ email, onChange, onSubmit }) {
+
   return(
     <form onSubmit={onSubmit}>
       <div className='flex-col'>
         <label htmlFor="">Email address</label>
-        <input onChange={onChange} placeholder='ash@loremcompany.com' type="text" value={email}/>
+        <input onChange={onChange} placeholder='ash@loremcompany.com' type="email" value={email} required/>
       </div>
       <div>
         <Button>Subscribe to monthly newsletter</Button>
