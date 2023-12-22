e7c8bf7ea3aef1796034ad10f2354696220d0758
Anna Duong <aduong9417@gmail.com>
2023-12-22 12:08:44 -0800

    refactor code for conditional rendering of SubscribePage and SuccessPage

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import Illustration from '../images/illustration-sign-up-desktop.svg';
 function App() {
   const [ email, setEmail ] = useState('');
   const [ formSubmitted, setFormSubmitted ] = useState(false);
-  const [ showSubscribe, setShowSubscribe ] = useState(true);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,17 +14,15 @@ function App() {
   const handleOnSubmit = (e) => {
     e.preventDefault();
     setFormSubmitted(true);
-    setShowSubscribe(false);
   };
   
 
   return(
     <div className='container flex-center'>
-      {showSubscribe && <div className='main-content'>
+      {formSubmitted ? <SuccessPage email={email} /> : <div className='main-content'>
         <SubscribePage email={email} onChange={handleEmailChange} onSubmit={handleOnSubmit}/>
         <img id='illustration' src={Illustration} alt="illustration" />
       </div>}
-      {formSubmitted && <SuccessPage email={email} />}
     </div>
   )
 }
